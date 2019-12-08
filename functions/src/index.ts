d5715cccef31065a9d9a6b4d35181adf242889c3
Matej Kriz <matej@matejkriz.cz>
2019-12-08 19:46:55 +0100

    increase memory limit and timeout for generating thumbnails

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import { VALID_MEMORY_OPTIONS } from 'firebase-functions';
 import { handleThumbnails } from './thumbnails';
 
 import * as admin from 'firebase-admin';
 admin.initializeApp();
+
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: VALID_MEMORY_OPTIONS[4],
+};
 
 /**
  * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
@@ -10,6 +16,7 @@ admin.initializeApp();
  * After the thumbnail has been generated and uploaded to Cloud Storage,
  * we write the public URL to the Firebase Realtime Database.
  */
-exports.generateThumbnail = functions.storage
-  .object()
+exports.generateThumbnail = functions
+  .runWith(runtimeOpts)
+  .storage.object()
   .onFinalize(handleThumbnails);
